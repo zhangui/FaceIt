4c27834eb6cbeee84c69fba3d95ba417bf929077
Yang Zhang <yang10.zhang@gmail.com>
2017-05-18 20:38:11 -0400

    any message

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -13,20 +13,32 @@ class FaceView: UIView {
     
     // Public API
     
+    // 1.0 is full smile and -1.0 is full frown
     @IBInspectable
-    var mouthCurvature: Double = 0.5 // 1.0 is full smile and -1.0 is full frown
+    var mouthCurvature: Double = 0.5 { didSet { setNeedsDisplay() } }
     
     @IBInspectable
-    var eyesOpen: Bool = true
+    var eyesOpen: Bool = true { didSet { setNeedsDisplay() } }
     
     @IBInspectable
-    var scale: CGFloat = 0.9
+    var scale: CGFloat = 0.9 { didSet { setNeedsDisplay() } }
     
     @IBInspectable
-    var lineWidth: CGFloat = 5.0
+    var lineWidth: CGFloat = 5.0 { didSet { setNeedsDisplay() } }
     
     @IBInspectable
-    var color: UIColor = UIColor.blue
+    var color: UIColor = UIColor.blue { didSet { setNeedsDisplay() } }
+    
+    func changeScale(byReactingTo pinchRecognizer: UIPinchGestureRecognizer)
+    {
+        switch pinchRecognizer.state {
+        case .changed,.ended:
+            scale *= pinchRecognizer.scale
+            pinchRecognizer.scale = 1
+        default:
+            break
+        }
+    }
     
     // Private Implementation
     
