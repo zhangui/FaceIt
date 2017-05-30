e9cb4fa6916c3ad3259600bfaf12d5bdea7325ac
Yang Zhang <yang10.zhang@gmail.com>
2017-05-30 12:10:13 -0400

    animates the face

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -18,16 +18,27 @@ class FaceView: UIView {
     var mouthCurvature: Double = 0.5 { didSet { setNeedsDisplay() } }
     
     @IBInspectable
-    var eyesOpen: Bool = true { didSet { setNeedsDisplay() } }
+    var eyesOpen: Bool = true { didSet {
+        leftEye.eyesOpen = eyesOpen
+        rightEye.eyesOpen = eyesOpen
+    } }
     
     @IBInspectable
     var scale: CGFloat = 0.9 { didSet { setNeedsDisplay() } }
     
     @IBInspectable
-    var lineWidth: CGFloat = 5.0 { didSet { setNeedsDisplay() } }
+    var lineWidth: CGFloat = 5.0 { didSet {
+        leftEye.lineWidth = lineWidth
+        rightEye.lineWidth = lineWidth
+        setNeedsDisplay()
+    } }
     
     @IBInspectable
-    var color: UIColor = UIColor.blue { didSet { setNeedsDisplay() } }
+    var color: UIColor = UIColor.blue { didSet {
+        leftEye.color = color
+        rightEye.color = color
+        setNeedsDisplay()
+    } }
     
     func changeScale(byReactingTo pinchRecognizer: UIPinchGestureRecognizer)
     {
@@ -58,42 +69,74 @@ class FaceView: UIView {
         return CGPoint(x: bounds.midX, y: bounds.midY)
     }
     
+    // MARK: Eyes
+    
     private enum Eye {
         case left
         case right
     }
     
-    private func pathForEye(_ eye: Eye) -> UIBezierPath
-    {
-        func centerOfEye(_ eye: Eye) -> CGPoint {
-            let eyeOffset = skullRadius / Ratios.skullRadiusToEyeOffset
-            var eyeCenter = skullCenter
-            eyeCenter.y -= eyeOffset
-            eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset
-            return eyeCenter
-        }
-        
-        let eyeRadius = skullRadius / Ratios.skullRadiusToEyeRadius
-        let eyeCenter = centerOfEye(eye)
-        let path: UIBezierPath
-        
-        if eyesOpen {
-            path = UIBezierPath(
-                arcCenter: eyeCenter,
-                radius: eyeRadius,
-                startAngle: 0,
-                endAngle: CGFloat.pi * 2,
-                clockwise: true
-            )
-        } else {
-            path = UIBezierPath()
-            path.move(to: CGPoint(x: eyeCenter.x - eyeRadius, y: eyeCenter.y))
-            path.addLine(to: CGPoint(x: eyeCenter.x + eyeRadius, y: eyeCenter.y))
-        }
-        
-        path.lineWidth = lineWidth
-        return path
+//    private func pathForEye(_ eye: Eye) -> UIBezierPath
+//    {
+//
+//        let eyeRadius = skullRadius / Ratios.skullRadiusToEyeRadius
+//        let eyeCenter = centerOfEye(eye)
+//        let path: UIBezierPath
+//        
+//        if eyesOpen {
+//            path = UIBezierPath(
+//                arcCenter: eyeCenter,
+//                radius: eyeRadius,
+//                startAngle: 0,
+//                endAngle: CGFloat.pi * 2,
+//                clockwise: true
+//            )
+//        } else {
+//            path = UIBezierPath()
+//            path.move(to: CGPoint(x: eyeCenter.x - eyeRadius, y: eyeCenter.y))
+//            path.addLine(to: CGPoint(x: eyeCenter.x + eyeRadius, y: eyeCenter.y))
+//        }
+//        
+//        path.lineWidth = lineWidth
+//        return path
+//    }
+    
+    // eyes are shown using EyeView subviews now instead
+    // of being drawn by draw(rect:)
+    
+    private func centerOfEye(_ eye: Eye) -> CGPoint {
+        let eyeOffset = skullRadius / Ratios.skullRadiusToEyeOffset
+        var eyeCenter = skullCenter
+        eyeCenter.y -= eyeOffset
+        eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset
+        return eyeCenter
     }
+    
+    private lazy var leftEye: EyeView = self.createEye()
+    private lazy var rightEye: EyeView = self.createEye()
+    
+    private func createEye() -> EyeView {
+        let eye = EyeView()
+        eye.isOpaque = false
+        eye.color = color
+        eye.lineWidth = lineWidth
+        addSubview(eye)
+        return eye
+    }
+    
+    private func positionEye(_ eye: EyeView, center: CGPoint) {
+        let size = skullRadius / Ratios.skullRadiusToEyeRadius * 2
+        eye.frame = CGRect(origin: CGPoint.zero, size: CGSize(width: size, height: size))
+        eye.center = center
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        positionEye(leftEye, center: centerOfEye(.left))
+        positionEye(rightEye, center: centerOfEye(.right))
+    }
+    
+    // MARK: Mouth
     
     private func pathForMouth() -> UIBezierPath
     {
@@ -121,6 +164,8 @@ class FaceView: UIView {
         return path
     }
     
+    // MARK: Skull
+    
     private func pathForSkull() -> UIBezierPath {
         let path = UIBezierPath(
             arcCenter: skullCenter,
@@ -134,11 +179,13 @@ class FaceView: UIView {
         return path
     }
     
+    // MARK: Drawing
+    
     override func draw(_ rect: CGRect) {
         color.set()
         pathForSkull().stroke()
-        pathForEye(.left).stroke()
-        pathForEye(.right).stroke()
+//        pathForEye(.left).stroke()
+//        pathForEye(.right).stroke()
         pathForMouth().stroke()
     }
     
--- FaceIt/FaceViewController.swift
+++ FaceIt/FaceViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FaceViewController: VCLLoggingViewController {
     var expression = FacialExpression(eyes: .closed, mouth: .frown) {
         didSet {
             updateUI() }
@@ -34,9 +34,9 @@ class ViewController: UIViewController {
             let handler = #selector(FaceView.changeScale(byReactingTo:))
             let pinchRecognizer = UIPinchGestureRecognizer(target: faceView, action: handler)
             faceView.addGestureRecognizer(pinchRecognizer)
-            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byReactingTo:)))
-            tapRecognizer.numberOfTapsRequired = 1
-            faceView.addGestureRecognizer(tapRecognizer)
+//            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byReactingTo:)))
+//            tapRecognizer.numberOfTapsRequired = 1
+//            faceView.addGestureRecognizer(tapRecognizer)
             let swipeUpRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(increaseHappiness))
             swipeUpRecognizer.direction = .up
             faceView.addGestureRecognizer(swipeUpRecognizer)
@@ -62,6 +62,45 @@ class ViewController: UIViewController {
             let eyes: FacialExpression.Eyes = (expression.eyes == .closed) ? .open : .closed
             expression = FacialExpression(eyes: eyes, mouth: expression.mouth)
         }
+    }
+    
+    // MARK: Head Shake
+    
+    private struct HeadShake {
+        static let angle = CGFloat.pi/6                 // radians
+        static let segmentDuration: TimeInterval = 0.5  // each head shake has 3 segments
+    }
+    
+    private func rotateFace(by angle: CGFloat) {
+        faceView.transform = faceView.transform.rotated(by: angle)
+    }
+    
+    private func shakeHead() {
+        UIView.animate(
+            withDuration: HeadShake.segmentDuration,
+            animations: { self.rotateFace(by: HeadShake.angle) },
+            completion: { finished in
+                if finished {
+                    UIView.animate(
+                        withDuration: HeadShake.segmentDuration,
+                        animations: { self.rotateFace(by: -HeadShake.angle*2) },
+                        completion: { finished in
+                            if finished {
+                                UIView.animate(
+                                    withDuration: HeadShake.segmentDuration,
+                                    animations: { self.rotateFace(by: HeadShake.angle) }
+                                )
+                            }
+                    }
+                    )
+                }
+        }
+        )
+    }
+    
+    
+    @IBAction func shakeHead(_ sender: Any) {
+        shakeHead()
     }
 }
 
